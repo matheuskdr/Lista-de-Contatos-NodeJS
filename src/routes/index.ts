82547f956f3a6139c9b1fd4003a6aac2a982f39a
matheuskdr <matheusgabriellkpo@gmail.com>
2025-08-12 11:32:09 -0300

    refactored code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import { readFile, writeFile } from 'fs/promises';
-
-const dataSource = './data/list.txt';
+import { createContact, deleteContact, getContacts } from '../services/contact.js';
 
 const router = express.Router();
 
@@ -12,25 +10,13 @@ router.post('/contato', async (req, res) => {
         return res.json({ error: 'Nome precisa ter no minimo 2 caracteres.' });
     }
 
-    let list: string[] = [];
-    try {
-        const data = await readFile(dataSource, 'utf8');
-        list = data.split('\n');
-    } catch(err) {}
-
-    list.push(name);
-    await writeFile(dataSource, list.join('\n'));
+    await createContact(name);
 
     res.status(201).json({ contato: name });
 });
 
 router.get('/contatos', async (req, res) => {
-    let list: string[] = [];
-    try {
-        const data = await readFile(dataSource, 'utf8');
-        list = data.split('\n');
-    } catch(err) {}
-
+    let list = await getContacts();
     res.json({ contatos: list });
 });
 
@@ -41,15 +27,7 @@ router.delete('/contato', async (req, res) => {
         return res.json({ error: 'Precisa mandar um nome para excluir.' });
     }
 
-    let list: string[] = [];
-    try {
-        const data = await readFile(dataSource, 'utf8');
-        list = data.split('\n');
-    } catch(err) {}
-
-    list = list.filter(item => item.toLowerCase() !== (name as string).toLowerCase());
-
-    await writeFile(dataSource, list.join('\n'));
+    await deleteContact(name as string);
 
     res.json({ contato: name });
 });
